749e8809ccce8ed5cc4bca114fde7e75b863e2f1
Sammaiah-Guguloth <gugulothsammaiah5@gmail.com>
2026-04-05 00:28:19 +0530

    fix: restore dev stack trace & full response schemas to swagger docs

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -6,7 +6,9 @@ const globalErrorHandler = (err, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     res.status(err.statusCode).json({
       status: err.status,
+      error: err,
       message: err.message,
+      stack: err.stack,
     });
   } else {
     res.status(err.statusCode).json({
